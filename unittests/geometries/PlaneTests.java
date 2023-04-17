f4020bc817481d81562ace6dd94b1435a282bd06
Avidan <avidanm25@gmail.com>
2023-04-17 14:40:09 +0300

    add junit and some tests

--- unittests/geometries/PlaneTests.java
+++ unittests/geometries/PlaneTests.java
@@ -13,11 +13,11 @@ class PlaneTests {
     void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
         // TC01: Test that the normal is correct
-        Plane plane = new Plane(new Point(1, 2, 3), new Vector(4, 5, 6));
+        Plane plane = new Plane(new Point(1, 2, 3), new Point(4, 5, 6), new Point(7, 8, 9));
         // ensure there are no exceptions
         assertDoesNotThrow(() -> plane.getNormal(new Point(1, 2, 3)), "");
         // ensure the normal is correct
-        assertEquals(new Vector(4, 5, 6).normalize(), plane.getNormal(), "ERROR: Bad normal to plane");
+        assertEquals(new Vector(0, 0, 1), plane.getNormal(new Point(1, 2, 3)), "ERROR: Bad normal");
         // ensure the normal length is 1
         assertTrue(isZero(plane.getNormal().length() - 1), "ERROR: Bad normal!=1");
         // ensure there are no exceptions
