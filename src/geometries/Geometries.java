7934a5ad7a905b510518e8f695c6ee33a0c5af55
ziv20 <116748780+zivf23@users.noreply.github.com>
2023-04-30 22:36:30 +0300

    add implanmnt findIntsersections to Geometries.java

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -8,17 +8,16 @@ import java.util.List;
 /**
  * Intersectable interface represents a geometry in 3D Cartesian coordinate system
  */
-public class Geometries implements intersectable
-
-interface intersectable {
-    private List<Intersectable> _intersectables;
+public class Geometries implements Intersectable
+{
+     List<Intersectable> _intersectables;
 
     public void Geometries() {
-        _intersectables = new LinkedList<>();
+        _intersectables = new LinkedList<Intersectable>();
     }
 
     public void Geometries(Intersectable... geometries) {
-        _intersectables = new LinkedList<>();
+        _intersectables = new LinkedList<Intersectable>();
         add(geometries);
     }
 
@@ -28,8 +27,15 @@ interface intersectable {
         }
     }
 
-    public static List<Point> findIntsersections(Ray ray){
-        return null;
+    public List<Point> findIntsersections(Ray ray){
+        List<Point> result = new LinkedList<>();
+        for (var geometry: _intersectables){
+            List<Point> temp = geometry.findIntsersections(ray);
+            if (temp != null){
+                result.addAll(temp);
+        }
+        return result;
+    }
     }
 
 }
