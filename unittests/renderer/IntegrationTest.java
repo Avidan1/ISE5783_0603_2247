c2164b004bff808ab2283e1cf18cad9db6985ec0
Avidan <avidanm25@gmail.com>
2023-05-08 10:52:15 +0300

    test sphere

--- unittests/renderer/IntegrationTest.java
+++ unittests/renderer/IntegrationTest.java
@@ -15,6 +15,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IntegrationTest {
     Camera camera = new Camera(new Point(0, 0, 0), new Vector(0, 1, 0), new Vector(0, 0, -1));
 
+    int rayIntersectionLoop(Sphere sphere, Camera camera) {
+        int width = 3;
+        int height = 3;
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                count = count + sphere.findIntersections(camera.constructRay(width, height, i, j)).size();
+            }
+        }
+        return count;
+    }
+
     /**
      * *  testing the integration of camera and sphere
      **/
@@ -30,25 +42,27 @@ public class IntegrationTest {
         Camera camera18 = new Camera(new Point(0, 0, 0.5), new Vector(0, 1, 0), new Vector(0, 0, -1));
         camera18.setVPSize(3d, 3d);
         camera18.setDistance(1);
-        assertEquals(18, sphere18.findIntersections(camera18.constructRay(3, 3, 0, 0)).size(), "wrong number of intersections");
+        assertEquals(18, rayIntersectionLoop(sphere18, camera18), "wrong number of intersections");
         //TC03: 10 intersection points
         Sphere sphere10 = new Sphere(new Point(0, 0, -2), 2);
-        assertEquals(10, sphere10.findIntersections(camera18.constructRay(3, 3, 0, 0)).size(), "wrong number of intersections");
+        assertEquals(10, rayIntersectionLoop(sphere10,camera18), "wrong number of intersections");
         //TC04: 9 intersection points
         Sphere sphere9 = new Sphere(new Point(0, 0, -1), 4);
-        assertEquals(9, sphere9.findIntersections(camera18.constructRay(3, 3, 0, 0)).size(), "wrong number of intersections");
+        assertEquals(9, rayIntersectionLoop(sphere9,camera18), "wrong number of intersections");
         //TC05: 0 intersection points
         Sphere sphere0 = new Sphere(new Point(0, 0, 1), 0.5);
-        assertEquals(0, sphere0.findIntersections(camera18.constructRay(3, 3, 0, 0)).size(), "wrong number of intersections");
+        assertEquals(0, rayIntersectionLoop(sphere0,camera18), "wrong number of intersections");
 
     }
-// testing the integration of camera and triangle
+
+    // testing the integration of camera and triangle
     @Test
     void triangleIntegrationTest() {
         //
 
     }
-// testing the integration of camera and plane
+
+    // testing the integration of camera and plane
     @Test
     void planeIntegrationTest() {
 
