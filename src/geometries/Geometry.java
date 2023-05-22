477fc1207e0493ab30ee4a41a2428e14c787e405
ziv20 <116748780+zivf23@users.noreply.github.com>
2023-05-22 16:00:05 +0300

    add

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,6 +1,7 @@
 package geometries;
 
 import primitives.Color;
+import primitives.Material;
 import primitives.Point;
 import primitives.Vector;
 
@@ -8,25 +9,56 @@ import primitives.Vector;
  * Geometry interface represents a geometry in 3D Cartesian coordinate system
  */
 public abstract class Geometry extends Intersectable {
+    //***************fields**************//
     /**
      * The emission color of the geometry.
      */
-    protected Color emission=Color.BLACK;
+    protected Color emission = Color.BLACK;
 
     /**
      * Getter for the emission color of the geometry.
+     *
      * @return The emission color of the geometry.
      */
-    public Color getEmission(){return this.emission;}
+    public Color getEmission() {
+        return this.emission;
+    }
+
+    /**
+     * Getter for the material of the geometry.
+     *
+     * @return The material of the geometry.
+     */
+    public Material getMaterial() {
+        return material;
+    }
+
+    /**
+     * setter for the material of the geometry.
+     *
+     * @param material The material of the geometry to set.
+     * @return The geometry object with the updated material.
+     */
+    public Geometry setMaterial(Material material) {
+        this.material = material;
+        return this;
+    }
+
     /**
      * Setter for the emission color of the geometry.
+     *
      * @param emission The emission color of the geometry.
      * @return The geometry.
      */
-    public Geometry setEmission(Color emission){
-        this.emission=emission;
+    private Material material = new Material();
+
+    //**************setters**************//
+    public Geometry setEmission(Color emission) {
+        this.emission = emission;
         return this;
     }
+    //**************operations***********//
+
     /**
      * Calculate the normal vector to the geometry at the specified point.
      *
